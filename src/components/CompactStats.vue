444b7b0642677b5b4d5d980314d7a677164edd14
Sultepronit <sultepronit@gmail.com>
2024-06-13 12:24:48 +0300

    improved design

--- src/components/CompactStats.vue
+++ src/components/CompactStats.vue
@@ -13,37 +13,45 @@ const selected = ref(0);
 <section class="block">
     <div class="detailed-stats">
         <p class="title-date">{{ ukrainianDate(list[selected].date) }}</p>
-        <div class="stats-item">
+        <div class="stats-item columns">
             <p>Веронічка</p>
-            <p class="number">{{ list[selected].vira.balance }}</p>
+            <BigNumber :value="list[selected].vira.balance" />
             <BalanceChange :value="list[selected].vira.balanceChange" />
         </div>
-        <div class="stats-item">
+        <div class="stats-item columns">
             <p>Батько</p>
-            <p class="number">{{ Math.round(list[selected].stefko.balance) }}</p>
+            <BigNumber :value="list[selected].stefko.balance" />
             <BalanceChange :value="list[selected].stefko.change" />
         </div>
-        <div class="stats-item">
+        <div class="stats-item columns">
             <p>готівка</p>
-            <p class="number">{{ list[selected].common.cash.balance }}</p>
+            <BigNumber :value="list[selected].common.cash.balance" />
             <BalanceChange :value="list[selected].common.cash.change" />
         </div>
-        <div class="stats-item">
-            <p>USD</p>
-            <p class="number">{{ list[selected].common.usd.balance.balance }} / {{ list[selected].common.usd.uah }}</p>
+
+        <div class="stats-item columns">
+            <p>
+                USD
+                <span class="rate">{{ list[selected].common.usd.rate.balance.toFixed(2) }}</span>
+            </p>
+            <div class="usd">
+                <p>{{ list[selected].common.usd.balance.balance }} /&nbsp;</p>
+                <BigNumber :value="list[selected].common.usd.uah" />
+            </div>
             <BalanceChange :value="list[selected].common.usd.change" />
         </div>
-        <div class="stats-item">
+
+        <div class="stats-item columns">
             <p>баланс</p>
             <BigNumber :value="list[selected].balance" />
             <BalanceChange :value="list[selected].change" />
         </div>
-        <div class="stats-item">
+        <div class="stats-item columns">
             <p>доходи</p>
             <p></p>
             <BalanceChange :value="list[selected].income.sum" />
         </div>
-        <div class="stats-item">
+        <div class="stats-item columns">
             <p>розходи</p>
             <p></p>
             <BalanceChange :value="list[selected].change - list[selected].income.sum" />
@@ -54,7 +62,7 @@ const selected = ref(0);
         <div
             v-for="(day, index) in list"
             :key="index"
-            class="list-item"
+            class="list-item columns"
             :class="{selected: selected === index}"
             @click="selected = index"
         >
@@ -76,15 +84,18 @@ const selected = ref(0);
     font-size: 1.2em;
     font-weight: bold;
 }
-.stats-item {
+.columns {
     display: grid;
     grid-template-columns: 6fr 5fr 4fr;
+}
+.stats-item {
     background: #abf6ff;
     margin: 0.2em;
     padding-inline: 0.2em;
 }
-.number {
-    text-align: right;
+.usd {
+    display: flex;
+    justify-content: end;
 }
 .list {
     margin-top: 0.7rem;
@@ -92,8 +103,6 @@ const selected = ref(0);
     overflow: auto;
 }
 .list-item {
-    display: grid;
-    grid-template-columns: 6fr 5fr 4fr;
     margin: 0.4rem 0.1rem;
     padding: 0.3em;
     border-radius: 0.4rem;
@@ -101,5 +110,8 @@ const selected = ref(0);
 }
 .selected {
     background-color: #b5ffb5;
+}
+.rate {
+    color: green;
 }
 </style>
