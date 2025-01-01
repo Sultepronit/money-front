f6843783e444644d71eaee20b6d733ed8042694b
Sultepronit <sultepronit@gmail.com>
2025-01-01 13:42:39 +0200

    little changes

--- src/components/BalanceChart.vue
+++ src/components/BalanceChart.vue
@@ -8,11 +8,11 @@ import { LineSet } from '@/utils/chartDataSets.js';
 const chartData = computed(() => {
     return {
         datasets: [
+            new LineSet('Батько', null, 'blue', 3, data, ['stefko', 'balance']),
             new LineSet('Веронічка', null, '#ff00ff', 3, data, ['vira', 'balance']),
             new LineSet('готівка', null, 'rgba(0, 0, 0, 0.4)', 5, data, ['common', 'cash', 'balance']),
-            new LineSet('батько', null, 'blue', 3, data, ['stefko', 'balance']),
-            new LineSet('баланс', 'rgba(0, 255, 0, 0.3)', 'green', 2, data, ['balance']),
-            new LineSet('$            ', null, 'rgba(0, 255, 0, 1)', 5, data, ['common', 'usd', 'uah']),
+            new LineSet('$', null, 'rgba(0, 255, 0, 1)', 5, data, ['common', 'usd', 'uah']),
+            new LineSet('сума        ', 'rgba(0, 255, 0, 0.3)', 'green', 2, data, ['balance']),
         ]
     }
 });
